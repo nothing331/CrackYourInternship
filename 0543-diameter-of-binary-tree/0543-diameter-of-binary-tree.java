5634622e22c92d0a47fa9ddf8d69e3097be11d28
Ayush <75002721+nothing331@users.noreply.github.com>
2023-10-25 01:27:12 +0530

    Time: 0 ms (100%), Space: 41.5 MB (54.93%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -9,38 +9,24 @@
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
- *         this.right = right;
+ *         this.right = right; 
  *     }
  * }
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
+        int [] dia = new int[1];
+        height(root, dia);
+        return dia[0];
 
-        // Create an array to hold the diameter of the tree
-        int diameter[] = new int[1];
-
-        // Recursively calculate the height of the tree and update the diameter array
-        height(root,diameter);
-
-        // Return the diameter of the tree
-      return diameter[0];
     }
-
-    public int height(TreeNode root, int diameter[]){
-        
-        // Base case: if the root is null, the height is 0
+    public int height(TreeNode root, int []dia){
         if(root == null){
             return 0;
         }
-
-        // Recursively calculate the height of the left and right subtrees
-        int left = height(root.left,diameter);
-        int right = height(root.right,diameter);
-
-        // Update the diameter array by taking the maximum diameter that passes through the current node
-        diameter[0] = Math.max(diameter[0],left + right);
-
-        // Return the maximum depth of the current node by adding 1 to the maximum depth of its deepest subtree
-        return Math.max(left,right)+1;
+        int left=height(root.left, dia);
+        int right = height(root.right, dia);
+        dia[0] = Math.max(dia[0],right+left);
+        return 1+Math.max(right,left);
     }
 }
